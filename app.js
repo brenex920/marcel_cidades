acd9269382dea1ce8ecc4421f238ed3a54516a77
Breno Machado <machado.breno127@gmail.com>
2025-09-28 18:41:24 -0300

    comcluido

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const {request, get} = require('http')
 
 const dados = require('./modulo/funcoes.js')
 
+
 //define a porta padrao da API, se for um servidor em nuvem nao tem acesso a porta
         //en execuçao local podemos definir uma porta livre
 const PORT =  process.PORT || 8090;
@@ -44,49 +45,86 @@ app.use((request, response, next)=>{
 
 //EndPoints
 app.get('/v1/estado/', function(request, response){
-
     let estados = dados.getAllEstados();
-    response.status(estados.statuscode)
-    response.json(estados)
 
-})
+    response.status(estados.statuscode);
+
+    if (estados.status == true) {
+        let resultado = {
+            uf: estados.uf,
+            quantidade: estados.quantidade
+        };
+
+        response.json(resultado);
+    } else {
+        response.json({ messageError: estados.erro });
+    }
+});
 
 //request recebe os dados da API
 //respose envia os dados na API
 
 app.get('/v1/estado/:uf', function(request, response){
     let sigla = request.params.uf
-    console.log(sigla)
-})
-
-app.get('/api/filtros/estados', (req, res) => {
-    const resultado = getEstadoBySigla ();
-
-    res = console.log('deu ceto')
+    let resultado = dados.getFiltroEstado(sigla)
     
+    response.status(resultado.statuscode);
+
+    console.log(resultado);
+    
+    if (resultado.statuscode == 200) {
+        response.json(resultado.estado);
+    } else {
+        response.json(resultado);
+    }
 });
 
+app.get('/v1/estado/filtro/:uf', function(req, res){
+
+    let sigla = req.params.uf
+    let resultado = dados.getEstadoBySigla(sigla)
+    
+    res.status(resultado.statuscode);
+
+    if(resultado.statuscode == 200) {
+        res.json(resultado.estado)
+    }else{
+        res.json(resultado)
+    }
+    
+
+})
+app.get('/v1/estado/capital/:uf', function(request, response){
+    let capital = request.params.uf
+    let res = dados.getCapitalBysigla(capital)
+
+    response.json(res);
+})
+
+app.get('/v1/regiao/estado/:regiao', function(request, response){
+    let regiaoEstados = request.params.regiao;
+
+    let resultado = dados.getEstadosByregiao(regiaoEstados);
+    
+    response.json(resultado);
+    console.log(resultado);
+})
+app.get('/v1/estado/estados/:uf', function(request, response){
+    let estado = request.params.uf
+
+    let resultado = dados.getCidadesBySigla(estado)
+    response.json(resultado)
+
+})
+app.get('/v1/pais/estados/capital/', function(request, respose){
+
+
+    let resultado = dados.getEstadoIsCapitalByCountry()
+    respose.json(resultado)
+})
 
 //start da API
 app.listen(PORT, function(){
-    console.log('recebido')
-})
-app.get('/v1/estado/filtro/:uf', function(request, response){
-
-   
-    
-    
-
+    console.log("certo")
 })
 
-app.get('/v1/regiao/estado/:id', function(request, response){
-
-
-    let regiaoEstados = request.query.regiao
-    let sigla = request.query.uf
-    let id = request.params.id
-
-    console.log(regiaoEstados)
-    console.log(sigla)
-    console.log(id)
-})
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -5,70 +5,145 @@
  * versão: 1.0
  *//////////////////////////////////////////////////////////////////////////////////////
 
- //import do arquivo estados e cidades
+//import do arquivo estados e cidades
 const dados = require('./estados_cidades.js')
-const MESSAGE_ERROR = {status: false,statuscode: 500,devlopment:'Breno Dias Machado'}
- //retorna a lista de Estados 
- const getAllEstados = function() {
+const MESSAGE_ERROR = { status: false, statuscode: 500, devlopment: 'Breno Dias Machado', erro: "Erro" }
 
+//retorna a lista de Estados 
+const getAllEstados = function () {
+  //o forEach pega o array e repete ele chamando a sigla
+  //padrao do JSON que sera o retorno 
+  let message = { status: true, statuscode: 200, devlopment: 'Breno Dias Machado', uf: [] }
 
-    //o forEach pega o array e repete ele chamando a sigla
-    //padrao do JSON que sera o retorno 
-    let message = {status: true, statuscode: 200, devlopment: 'Breno Dias Machado',uf: []}
+  dados.listaDeEstados.estados.forEach(function (sigla) {
+    message.uf.push(sigla.sigla)
+  })
 
-    dados.listaDeEstados.estados.forEach(function(item){
-        message.uf.push(item.sigla)
-    })
+  //adiciona um novo elemento JSON 
+  message.quantidade = message.uf.length
 
-    //adiciona um novo elemento JSON 
-    message.quantidade = message.uf.length
+  //apaga um elemento existente no JSON
+  //delete message.status
 
-    //apaga um elemento existente no JSON
-    //delete message.status
+  if (message.uf.length > 0)
+    return message;//resultado da API 200
+  else
+    return MESSAGE_ERROR //resultado falso da API 500
+}
 
-    if(message.uf.length > 0 )
-        return message;//resultado da API 200
-    else
-        return MESSAGE_ERROR //resultado falso da API 500
-    
-    
- }
+const getFiltroEstado = function (sigla) {
+  let resultado = null;
 
-  const getFiltroEstado = function() {
+  dados.listaDeEstados.estados.forEach(estado => {
+    if (sigla == estado.sigla) {
+      let message = {
+        sigla: estado.sigla,
+        descricao: estado.nome,
+        capital: estado.capital,
+        regiao: estado.regiao
+      };
 
+      resultado = { estado: message, statuscode: 200 };
+    }
+  });
 
-   
-    
-        
-  }
+  if (resultado == null)
+    return MESSAGE_ERROR;
 
+  return resultado;
+}
+//retorna a capital do estado filtrando pela sigla
+const getCapitalBysigla = function (sigla) {
 
- //retorna dados do estado filtrando pela sigla
- const getEstadoBySigla = function(sigla) {
+  dados.listaDeEstados.estados.forEach(estado => {
+    if (sigla == estado.sigla) {
+      let message = {
+        uf: estado.sigla,
+        descricao: estado.nome,
+        capital: estado.capital
+      };
+      resp = message
+    }
+  })
+  return resp;
+}
 
-    sigla = console.log('feito')
-    
- }
- //retorna a capital do estado filtrando pela sigla
- const getCapitalBysigla = function(sigla) {
+//retorna a lista de estados filtrando pela região
+const getEstadosByregiao = function (regiao) {
+  let estadosRegiao = [];
 
- }
+  let resultado = {
+    regiao: regiao,
+    estados: [],
+  };
 
- //retorna a lista de estados filtrando pela região
- const getEstadosByregiao = function (regiao) {
+  dados.listaDeEstados.estados.forEach((estado) => {
+    if (estado.regiao == regiao) {
+      let temp = {
 
- }
+        uf: estado.nome,
+        descricao: estado.nome
+
+      }
+
+      estadosRegiao.push(temp);
+    }
+  });
+
+  resultado.estados = estadosRegiao;
+
+  return resultado;
+}
 //retorna a lista de estados que forma a capital de um pais filtrando pelo pais
-const getEstadoIsCapitalByCountry = function (pais) {
+const getEstadoIsCapitalByCountry = function () {
+
+  
+
+  let message = []
+
+  dados.listaDeEstados.estados.forEach((estado) => {
+    if (estado.capital_pais) {
+      message.push({
+        capitais: estado.capital_pais.capital,
+        uf: estado.uf,
+        capital: estado.capital,
+        descricao: estado.nome,
+        regiao: estado.regiao,
+        capital_pais_ano_inicio: estado.capital_pais.ano_inicio,
+        capital_pais_ano_termino: estado.capital_pais.ano_fim
+      })
+    }
+
+  })
+  return message;
+
 
 }
 //retorna a cidades existentes em um estado filtrando pela sigla
 const getCidadesBySigla = function (sigla) {
 
+  let message = null;
+
+
+  dados.listaDeEstados.estados.forEach((estado) => {
+    if (sigla == estado.sigla) {
+      message = {
+        uf: estado.sigla,
+        descricao: estado.nome,
+        quantidade: estado.cidades.length,
+        cidades: estado.cidades
+      }
+    }
+  })
+  return message;
 }
 
 
 module.exports = {
-    getAllEstados,
-    getFiltroEstado
+  getAllEstados,
+  getFiltroEstado,
+  getCapitalBysigla,
+  getEstadosByregiao,
+  getCidadesBySigla,
+  getEstadoIsCapitalByCountry
 }
